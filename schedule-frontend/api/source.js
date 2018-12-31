f91792df65ac673e323e88b57061f46d00ab5d47
丰硕 <fengshuochn@126.com>
2018-12-31 19:23:20 +0800

    refactor: fron-end

---
+++ schedule-frontend/api/source.js
@@ -0,0 +1,10 @@
+import { request } from '../utils/request';
+import { domain } from './domain';
+
+export function requestSource (data) {
+  return request({
+    url: `${domain}/source`,
+    method: 'POST',
+    ...data
+  }) 
+}
